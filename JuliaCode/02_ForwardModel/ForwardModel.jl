f7736f18e676353a4231d61efdef86ad16971de6
Avina Kalle <59176676+Avina-cK@users.noreply.github.com>
2022-10-21 21:50:32 +0200

    Add files via upload

---
+++ JuliaCode/02_ForwardModel/ForwardModel.jl
@@ -0,0 +1,22 @@
+using FFTW, LinearAlgebra, Plots, ColorSchemes, CSV, Tables, DataFrames
+# using ToeplitzMatrices;
+
+cd("C://Users/avina/Documents/2020-2022_MSc_MathMods/401_Thesis/JuliaCode/01_SettingUpTestObject_Probe")
+
+test_object_amp = CSV.read("TestObjectAmplitude.csv", DataFrame, header=false);
+test_object_phase = CSV.read("TestPhase.csv", DataFrame, header=false);
+test_probe = CSV.read("TestProbe.csv", DataFrame, header=false);
+
+obj_amp = Matrix(test_object_amp);
+obj_phase = Matrix(test_object_phase);
+object = obj_amp .* exp.(im .* obj_phase);
+probe = Matrix(test_probe);
+
+## Forward model
+#= |F[Pₘ(ω)∘O(ω)]|² = Iₘ(t)
+ where  ∘ is the Hadamard (or Schur) Product or element wise multiplication
+        Pₘ(ω) is the probe
+        O(ω) is the object
+=#
+prod_P_O = probe .* object;
+intensity = (abs.(fftshift(fft(prod_P_O)))).^2
