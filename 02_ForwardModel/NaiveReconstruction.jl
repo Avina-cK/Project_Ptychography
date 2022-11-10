f87cb38958cf2d51c4d7c1ed72ea79e85429cade
Avina Kalle <59176676+Avina-cK@users.noreply.github.com>
2022-11-10 03:01:04 +0100

    Update NaiveReconstruction.jl

--- 02_ForwardModel/NaiveReconstruction.jl
+++ 02_ForwardModel/NaiveReconstruction.jl
@@ -2,11 +2,20 @@
 using FFTW, LinearAlgebra, Plots, ColorSchemes, CSV, Tables, DataFrames
 # using ToeplitzMatrices;
 
-cd("C://Users/avina/Documents/2020-2022_MSc_MathMods/401_Thesis/JuliaCode/02_ForwardModel")
+cd(".../02_ForwardModel")
+include("ForwardModel.jl")
+
 #=
-imports:    x: domain
+imports:    ω: freq domain
+            Δω: resolution of ω
+            t: time domain
+            Δt: resolution of t
+
             object: object ∈ ℂ
+            obj_amp: object Amplitude
+            obj_phase: object phase
             Probe_set : set of dimension 1:2001 × -2:2
+            Int_Measured : set of measured intensities
 =#
 
 ## Forward model
@@ -34,23 +43,53 @@ end
 plot()
 colors =range(HSV(0,1,1), stop=HSV(-360,1,1), length=n)
 for i=1:n
-    plot!(x, normalize(angle.(Results[:,i])), alpha=0.2
+    plot!(ω, normalize(angle.(Results[:,i])), alpha=0.2
     , marker=true, markeralpha=0.3, markersize=2
     , markercolor=colors[i], markerstrokewidth=0
     , markerstrokecolor=colors[i], linecolor=colors[i]
     , label="NR, probe shift "*string(n_range[i]))
 end
-plot!(x, normalize(angle.(object)), marker=true
+plot!(ω, normalize(angle.(object)), marker=true
     , markeralpha=0.6, markersize=2, markerstrokewidth=0
     , markerstrokecolor=:orange, markercolor=:orange
     ,linecolor=:orange, label="Actual Object")
 plot!(legend=:topleft, legendtitle = "Naïve reconstruction (NR)"
-    , size=(1400,800)  , title="Comparing Phase")
-
+    , size=(1400,800)  , title="Comparing Phase", xlabel="ω")
 #savefig("ComparingPhase.png")
 
-plot(abs.(result./norm(result)), linealpha=0.8, marker=true, markeralpha=0.2, markersize=2, label="Absolute Value (Naïve reconstruction)",markerstrokecolor=:blue, markercolor=:blue, linewidth=2)
-plot!(abs.(object./norm(object)), linealpha=0.4, marker=true, markeralpha=0.8, markersize=2, label="Absolute Value (Object)",markerstrokecolor=:orange, markercolor=:orange, linewidth=2)
-plot!(legend=:top, ylabel="Absolute Value", size=(1400,800))
+plot()
+for i=1:n
+    plot!(ω, abs.(normalize(Results[:,i]))
+    , alpha=0.3, linewidth=2
+    , marker=true, markeralpha=0.3, markersize=2
+    , markercolor=colors[i], markerstrokewidth=0
+    , markerstrokecolor=colors[i], linecolor=colors[i]
+    , label="NR, probe shift "*string(n_range[i]))
+end
+plot!(ω, abs.(normalize(object)), linealpha=0.4, marker=true, markeralpha=0.8, markersize=2, label="Object",markerstrokecolor=:orange, markercolor=:orange, color=:orange, linewidth=2)
+plot!(legend=:top, title="Comparing Absolute Value", size=(1400,800), legendtitle="Naïve reconstruction(NR)", xlabel="ω")
 
-#savefig("ComparingAbsValue.png")
+savefig("ComparingAbsValue.png")
+
+
+# Next naïve step would be to take the average of all the results
+Avg_Result = vec(mean(Results, dims=2));
+plot(ω, abs.(normalize(Avg_Result)), linewidth=2
+    , label="NR, Avg(Result)")
+plot!(ω, abs.(normalize(object)), linewidth=2, label="Object")
+plot!(legend=:top, title="Comparing Absolute Value"
+    ,size=(1400,800), legendtitle="Naïve reconstruction(NR)"
+    , xlabel="ω", ylabel="|O(ω)|")
+savefig("ComparingAbsValue_AveragevsTrue.png")
+
+plot(ω, (normalize(angle.(Avg_Result)))
+    , label="NR, Avg(Result)", linealpha=0.3
+    , marker=true, markeralpha=0.4
+    , markersize=2, markerstrokewidth=0)
+plot!(ω, (normalize(angle.(object))), label="Object"
+    , linewidth=2)
+plot!(legend=:topleft
+    , legendtitle = "Naïve reconstruction (NR)"
+    , size=(1400,800)  , title="Comparing Phase"
+    , xlabel="ω", ylabel ="Phase(Object)")
+savefig("ComparingPhase_AveragevsTrue.png")
