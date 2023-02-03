b2aabdb62c106abe16fcf88fee456891e924c141
Avina Kalle <59176676+Avina-cK@users.noreply.github.com>
2023-02-03 03:45:22 +0100

    Update 20230203_Gerchberg_SaxtonAlgorithm.jl

--- 03_KnownAmp_Int/20230203_Gerchberg_SaxtonAlgorithm.jl
+++ 03_KnownAmp_Int/20230203_Gerchberg_SaxtonAlgorithm.jl
@@ -2,6 +2,17 @@ using FFTW, LinearAlgebra, Plots, ColorSchemes, StatsBase
 
 ## Reference:
 #=
+
+    A practical algorithm for the determination
+    of phase from image and diffraction plane pictures
+        R. W. Gerchberg and W. O. Saxton,
+        1972
+        Optik
+        Vol. 35 
+        pp. 237–247.
+
+and
+
     Phase retrival algorithms: a comparison
         J.R. Fienup
         1 Aug 1982
