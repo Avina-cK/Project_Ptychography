ccd33e510898f61fccfed6ba88026c002cf5504b
Avina Kalle <59176676+Avina-cK@users.noreply.github.com>
2023-03-11 16:43:58 +0100

    Update CreatingLorentzian_TestObject_and_Probe.jl

--- 01_SettingUpTestObject_Probe/CreatingLorentzian_TestObject_and_Probe.jl
+++ 01_SettingUpTestObject_Probe/CreatingLorentzian_TestObject_and_Probe.jl
@@ -4,10 +4,11 @@ using OffsetArrays
 using Plots
 using Tables
 
+include(".../00_BasicFunctions/BasicFunctionsAndPlots.jl")
 cd(".../01_SettingUpTestObject_Probe")
 
 ω_end = 0.5;
-ω_res = 0.005;
+ω_res = 0.0005;
 ω = -ω_end:ω_res:ω_end;
 ω_l = (length(ω) - 1)/2;
 Δω = ω_res;
@@ -48,9 +49,16 @@ c₂ = centres[5];   # centre
 Γ₂ = Γ₋₁;   # width
 
 # Lorentzian Function
-Lorentzian(x, x₀, Γ) = (1/π).*((0.5 .* Γ) ./ (((x .- x₀).^2) .+ (0.5 .* Γ).^2));
-
-# peak -2
+"""
+    Lorentzian(x, x₀, Γ)
+    
+    x ∈ ℝⁿ is the domain
+    x₀ ∈ ℝ is the centre of the curve
+    Γ ∈ ℝ⁺ is the width of the curve
+"""
+function Lorentzian(x, x₀, Γ)
+    (1/π).*((0.5 .* Γ) ./ (((x .- x₀).^2) .+ (0.5 .* Γ).^2))
+end
 peak₋₂ = normalize(Lorentzian(ω, c₋₂, Γ₋₂));
 
 # peak -1
@@ -67,14 +75,16 @@ all_peaks = peak₋₂ .+ peak₋₁ .+ peak₁ .+ peak₂;
 objectAmplitude = normalize(all_peaks);    # Object Amplitude
 
 ## Object Phase
-#=
+
+"""
+    step_func(x, centres)
+
 creating a step function
                 ....
             ....
         ....
     ....
-=#
-
+"""
 function step_func(x, centres)
     n = length(x);
     y = zeros(n);
@@ -98,17 +108,12 @@ function step_func(x, centres)
     return y;
 end
 
-function Gaussian(x, a, b, c²)
-    a .* exp.(-((x .- b).^2)/(2.0 .* c²))
-end
-
-
 a = 1.0;    # height of peak
 b = 0.05;    # centre of peak
 c² = 0.02;   # c = standard deviation : controls width
 
-phase_gaus = normalize(Gaussian(ω, a, b.+(Δω), c²));
-
+#phase = normalize(Gaussian(ω, a, b.+(Δω), c²));
+#a = 10.0; phase = sinc_probe(ω, a);
 phase = step_func(ω, centres);
 
 ## Complete Complex Object
@@ -126,7 +131,8 @@ Function to define probe.
     m is the index of offset.
 """
 function probe(m, Δω)
-    normalize(Gaussian(ω, a, b.+(m*Δω), c²));
+    #normalize(Gaussian(ω, a, b.+(m*Δω), c²));  #old
+    sinc_probe(ω.+(m*Δω), 10.0);
 end
 
 # 
@@ -137,7 +143,7 @@ M_max = no. of probe readings. ideally odd.
 """
 function Probe_set_function(M_max)
     m_end = Int64(floor(M_max/2));
-    Δω = ω_end/(2*(M_max-1));
+    Δω = ω_end/((M_max-1));         # Old version: ω_end/(2*(M_max-1))
 
     Probe_set = zeros(length(ω), M_max) + im.*zeros(length(ω), M_max);
 
@@ -149,7 +155,8 @@ function Probe_set_function(M_max)
 end
 
 
-M_max = 13;  #no. of probe readings. ideally an odd number
+M_max = 5;  #no. of probe readings. ideally an odd number
+m_end = Int64(floor(M_max/2));
 Probe_set = Probe_set_function(M_max);
 
 ##
@@ -157,38 +164,52 @@ Probe_set = Probe_set_function(M_max);
 obj_amp = objectAmplitude;
 obj_phase = phase;
 
+function plottingprobeset(probeset)
+    M_max=size(probeset)[2];
+    m_end = Int64(floor(M_max/2));
+    colors = range(HSV(0,1,1), stop=HSV(-300,1,1), length=M_max);
+    plot(size=(1800, 800),
+        xlabel="ω", ylabel="|Pm̃(ω)|");
+    for i=-m_end:m_end
+    plot!(ω, abs.(probeset[:,i]), label=i, linewidth=2 , color=colors[i+m_mid+1]);
+    end
+    p_Probeset = plot!(legendtitle="m̃", legendfontsize=10, legendtitlefontsize=15);
+    headerprobe = string.(-m_end:1:m_end);
+    return  plot(p_Probeset, title="|Pm̃(ω)| set", titlefontsize=15, left_margin = 5mm, bottom_margin = 5mm)
+end
+
 ## Writing and plotting
 
 #=
+using Tables, CSV
 
 CSV.write("TestDomain.csv", Tables.table(ω), header=false)
 
-p_objAmp = plot(ω, objectAmplitude, title="Object Amplitude", xlabel ="ω", ylabel="Obj(Amp)(ω)")
+p_objAmp = plot(ω, objectAmplitude, title="Object Amplitude"
+        , xlabel ="ω", ylabel="|O(ω)|"
+        , linewidth=2.5, label=false, color=:green, titlefontsize=15)
 #CSV.write("TestObjectAmplitude.csv", Tables.table(objectAmplitude), header=false)
 
-p_phase = plot(ω, phase, title="Object Phase",  xlabel ="ω", ylabel="Obj(Phase)(ω)");
+p_phase = plot(ω, phase, title="Object Phase"
+        ,  xlabel ="ω", ylabel="θₒ(ω)"
+        , linewidth=2.5, color=:darkgreen, titlefontsize=15);
 #CSV.write("TestPhase.csv", Tables.table(phase), header=false)
 
 plot(real(object), imag(object));
 #CSV.write("TestObject.csv", Tables.table(object), header=false)
 
-p_probe=plot(ω, Probe_set[:,0], title="Probe", xlabel ="ω", ylabel="Probe(ω)")
-CSV.write("TestProbe.csv", Tables.table(vec(Probe_set[:,0])), header=false)
+p_probe=plot(ω, abs.(Probe_set[:,0]), title="Probe", xlabel ="ω", ylabel="Probe(ω)")
+#CSV.write("TestProbe.csv", Tables.table(vec(Probe_set[:,0])), header=false)
 
-plot(p_objAmp, p_phase, p_probe
-        ,layout=(1,3), legend=false, size = (1800, 600)
-        ,xlabel=" "
+plot(p_objAmp, p_phase
+        #, p_probe
+    , layout=(1,2), legend=false, size = (1800,800)
+    , left_margin = 5mm
+    , bottom_margin = 5mm
     )
 
 savefig("ObjectAmplitude_Phase_Probe.png")
 
-plot(size=(1400, 1000));
-for i=-m_end:m_end
-    plot!(Probe_set[:,i], label=i, linewidth=2);
-end
-p_Probeset = plot!(legendtitle="m");
-headerprobe = string.(-m_end:1:m_end)
-CSV.write("TestProbeSet.csv", Tables.table(parent(Probe_set)), header=headerprobe)
-plot(p_Probeset)
+plottingprobeset(Probe_set)
 savefig("Probe_set.png")
 =#
